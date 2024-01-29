326e707336b64437dbc36b57ead05075a93db7f9
gxuud <igaoxian@foxmail.com>
2024-01-29 23:17:50 +0800

    feat: 增加依赖

---
+++ base.sh
@@ -0,0 +1,9 @@
+pnpm --filter './packages/core' install --save-dev mocha@^8.1.3
+pnpm --filter './packages/core' install --save colors@^1.4.0 commander@^6.1.0 dotenv@^8.2.0 minimist@^1.2.5 npminstall@^4.9.1 root-check@^1.0.0 semver@^7.3.2 user-home@^2.0.0
+pnpm --filter './packages/init' install --save-dev mocha@^8.1.3
+pnpm --filter './packages/init' install --save fs-extra@^9.0.1
+pnpm --filter './packages/publish' install --save-dev mocha@^8.1.3
+pnpm --filter './packages/publish' install --save colors@^1.4.0 socket.io-client@^2.3.1
+pnpm --filter './packages/replace' install --save ali-oss@^6.13.2 fs-extra@^9.1.0 glob@^7.1.6
+pnpm --filter './packages/utils' install --save-dev mocha@^8.1.3
+pnpm --filter './packages/utils' install --save axios@^0.20.0 cli-spinner@^0.2.10 ejs@^3.1.5 fs-extra@^9.0.1 glob@^7.1.6 inquirer@^7.3.3 kebab-case@^1.0.0 lodash@^4.17.20 npminstall@^4.9.1 npmlog@^4.1.2 semver@^7.3.2 simple-git@^2.20.1 socket.io-client@^2.3.1 terminal-link@^2.1.1 url-join@^4.0.1 user-home@^2.0.0
---
+++ scripts/README.md
@@ -0,0 +1,23 @@
+# scripts
+
+[![NPM version](https://img.shields.io/npm/v/scripts.svg?style=flat)](https://npmjs.org/package/scripts)
+[![NPM downloads](http://img.shields.io/npm/dm/scripts.svg?style=flat)](https://npmjs.org/package/scripts)
+
+## Install
+
+```bash
+$ pnpm install
+```
+
+```bash
+$ npm run dev
+$ npm run build
+```
+
+## Options
+
+TODO
+
+## LICENSE
+
+MIT
